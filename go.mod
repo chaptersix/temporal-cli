ee27e9e3ea0eb330205150838543f552c18676af
alex.stanfield <13949480+chaptersix@users.noreply.github.com>
2026-02-02 03:39:34 -0600

    use dev server from server repo

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/temporalio/cli
 
-go 1.25.0
+go 1.25.5
 
 require (
 	github.com/BurntSushi/toml v1.4.0
@@ -17,7 +17,7 @@ require (
 	github.com/stretchr/testify v1.10.0
 	github.com/temporalio/cli/cliext v0.0.0
 	github.com/temporalio/ui-server/v2 v2.42.1
-	go.temporal.io/api v1.60.0
+	go.temporal.io/api v1.61.1-0.20260128230845-c246540cf2ed
 	go.temporal.io/sdk v1.38.0
 	go.temporal.io/sdk/contrib/envconfig v0.1.0
 	go.temporal.io/server v1.30.0-148.4
@@ -30,6 +30,8 @@ require (
 )
 
 replace github.com/temporalio/cli/cliext => ./cliext
+
+replace go.temporal.io/server => ../temporal
 
 require (
 	cel.dev/expr v0.23.1 // indirect
@@ -59,6 +61,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cncf/xds/go v0.0.0-20250326154945-ae57f3c0d45f // indirect
 	github.com/coreos/go-oidc/v3 v3.13.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.6 // indirect
 	github.com/dgryski/go-farm v0.0.0-20240924180020-3414d57e47da // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/envoyproxy/go-control-plane/envoy v1.32.4 // indirect
@@ -119,6 +122,7 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/robfig/cron v1.2.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/sony/gobreaker v1.0.0 // indirect
@@ -131,8 +135,10 @@ require (
 	github.com/twmb/murmur3 v1.1.8 // indirect
 	github.com/uber-common/bark v1.3.0 // indirect
 	github.com/uber-go/tally/v4 v4.1.17 // indirect
+	github.com/urfave/cli/v2 v2.27.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	github.com/zeebo/errs v1.4.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/detectors/gcp v1.35.0 // indirect
--- internal/devserver/server.go
+++ internal/devserver/server.go
@@ -29,35 +29,13 @@ package devserver
 import (
 	"fmt"
 	"log/slog"
-	"math/rand"
-	"os"
-	"path/filepath"
-	"strconv"
-	"time"
 
 	uiserver "github.com/temporalio/ui-server/v2/server"
 	uiconfig "github.com/temporalio/ui-server/v2/server/config"
 	uiserveroptions "github.com/temporalio/ui-server/v2/server/server_options"
 	"go.temporal.io/api/enums/v1"
-	"go.temporal.io/server/common/authorization"
-	"go.temporal.io/server/common/cluster"
-	"go.temporal.io/server/common/config"
-	"go.temporal.io/server/common/dynamicconfig"
-	"go.temporal.io/server/common/membership/static"
-	"go.temporal.io/server/common/metrics"
-	sqliteplugin "go.temporal.io/server/common/persistence/sql/sqlplugin/sqlite"
-	"go.temporal.io/server/common/primitives"
-	"go.temporal.io/server/components/callbacks"
-	"go.temporal.io/server/components/nexusoperations"
-	"go.temporal.io/server/schema/sqlite"
-	sqliteschema "go.temporal.io/server/schema/sqlite"
 	"go.temporal.io/server/temporal"
-	"google.golang.org/grpc"
-	"gopkg.in/yaml.v3"
-)
-
-const (
-	localhost = "127.0.0.1"
+	serverdevserver "go.temporal.io/server/tools/devserver"
 )
 
 type StartOptions struct {
@@ -87,7 +65,6 @@ type StartOptions struct {
 	DynamicConfigValues   map[string]any
 	SearchAttributes      map[string]enums.IndexedValueType
 	LogConfig             func([]byte)
-	GRPCInterceptors      []grpc.UnaryServerInterceptor
 }
 
 type Server struct {
@@ -97,42 +74,56 @@ type Server struct {
 }
 
 func Start(options StartOptions) (*Server, error) {
-	// Validate
-	if options.FrontendIP == "" {
-		return nil, fmt.Errorf("missing frontend IP")
-	} else if options.FrontendPort == 0 {
-		return nil, fmt.Errorf("missing frontend port")
-	} else if len(options.Namespaces) == 0 {
-		return nil, fmt.Errorf("missing namespaces")
-	} else if options.Logger == nil {
+	// Validate CLI-specific options (server validates its own options)
+	if options.Logger == nil {
 		return nil, fmt.Errorf("missing logger")
-	} else if options.UIIP != "" && options.UIPort == 0 {
+	}
+	if options.UIIP != "" && options.UIPort == 0 {
 		return nil, fmt.Errorf("must provide UI port if UI IP is provided")
-	} else if options.ClusterID == "" {
-		return nil, fmt.Errorf("missing cluster ID")
-	} else if options.MasterClusterName == "" {
-		return nil, fmt.Errorf("missing master cluster name")
-	} else if options.CurrentClusterName == "" {
-		return nil, fmt.Errorf("missing current cluster name")
-	} else if options.InitialFailoverVersion == 0 {
-		return nil, fmt.Errorf("missing initial failover version")
 	}
 
-	if options.FrontendHTTPPort == 0 {
-		options.FrontendHTTPPort = MustGetFreePort(options.FrontendIP)
-	}
-
-	// Build servers
+	// Build UI server if needed
 	var ui *uiserver.Server
 	if options.UIIP != "" {
 		ui = options.buildUIServer()
 	}
-	server, logLevel, err := options.buildServer()
-	if err != nil {
-		return nil, err
+
+	// Create logger wrapper
+	logLevel := &slog.LevelVar{}
+	logLevel.Set(options.LogLevel)
+	logger := slogLogger{
+		log:   options.Logger,
+		level: logLevel,
 	}
 
-	// Start. We have to start UI server in background because it's start call is
+	// Build server options for the server repo's devserver
+	serverOpts := serverdevserver.Options{
+		FrontendIP:             options.FrontendIP,
+		FrontendPort:           options.FrontendPort,
+		Namespaces:             options.Namespaces,
+		ClusterID:              options.ClusterID,
+		MasterClusterName:      options.MasterClusterName,
+		CurrentClusterName:     options.CurrentClusterName,
+		InitialFailoverVersion: options.InitialFailoverVersion,
+		Logger:                 logger,
+		DatabaseFile:           options.DatabaseFile,
+		MetricsPort:            options.MetricsPort,
+		PProfPort:              options.PProfPort,
+		SqlitePragmas:          options.SqlitePragmas,
+		FrontendHTTPPort:       options.FrontendHTTPPort,
+		EnableGlobalNamespace:  options.EnableGlobalNamespace,
+		DynamicConfigValues:    options.DynamicConfigValues,
+		SearchAttributes:       options.SearchAttributes,
+		LogConfig:              options.LogConfig,
+	}
+
+	// Create the temporal server using the server repo's devserver
+	server, err := serverdevserver.New(serverOpts)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating server: %w", err)
+	}
+
+	// Start. We have to start UI server in background because its start call is
 	// blocking. Therefore we have no way to relay error out to users, so we just
 	// log and panic.
 	if ui != nil {
@@ -178,204 +169,4 @@ func (s *StartOptions) buildUIServer() *uiserver.Server {
 		CORS:                uiconfig.CORS{CookieInsecure: true},
 		HideLogs:            true,
 	}))
-}
-
-func (s *StartOptions) buildServer() (temporal.Server, *slog.LevelVar, error) {
-	opts, logLevel, err := s.buildServerOptions()
-	if err != nil {
-		return nil, nil, err
-	}
-	server, err := temporal.NewServer(opts...)
-	return server, logLevel, err
-}
-
-func (s *StartOptions) buildServerOptions() ([]temporal.ServerOption, *slog.LevelVar, error) {
-	// Build config and log it
-	conf, err := s.buildServerConfig()
-	if err != nil {
-		return nil, nil, err
-	} else if s.LogConfig != nil {
-		// We're going to marshal YAML
-		if b, err := yaml.Marshal(conf); err != nil {
-			s.Logger.Warn("Failed marshaling config for logging", "error", err)
-		} else {
-			s.LogConfig(b)
-		}
-	}
-
-	// Build common opts
-	logLevel := &slog.LevelVar{}
-	logLevel.Set(s.LogLevel)
-	logger := slogLogger{
-		log:   s.Logger,
-		level: logLevel,
-	}
-	authorizer, err := authorization.GetAuthorizerFromConfig(&conf.Global.Authorization)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed creating authorizer: %w", err)
-	}
-	claimMapper, err := authorization.GetClaimMapperFromConfig(&conf.Global.Authorization, logger)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed creating claim mapper: %w", err)
-	}
-	opts := []temporal.ServerOption{
-		temporal.WithConfig(conf),
-		temporal.ForServices(temporal.DefaultServices),
-		temporal.WithStaticHosts(map[primitives.ServiceName]static.Hosts{
-			primitives.FrontendService: static.SingleLocalHost(
-				fmt.Sprintf("%v:%v", localhost, conf.Services[string(primitives.FrontendService)].RPC.GRPCPort)),
-			primitives.MatchingService: static.SingleLocalHost(
-				fmt.Sprintf("%v:%v", localhost, conf.Services[string(primitives.MatchingService)].RPC.GRPCPort)),
-			primitives.HistoryService: static.SingleLocalHost(
-				fmt.Sprintf("%v:%v", localhost, conf.Services[string(primitives.HistoryService)].RPC.GRPCPort)),
-			primitives.WorkerService: static.SingleLocalHost(
-				fmt.Sprintf("%v:%v", localhost, conf.Services[string(primitives.WorkerService)].RPC.GRPCPort)),
-		}),
-		temporal.WithLogger(logger),
-		temporal.WithAuthorizer(authorizer),
-		temporal.WithClaimMapper(func(*config.Config) authorization.ClaimMapper { return claimMapper }),
-	}
-
-	dynConf := make(dynamicconfig.StaticClient, len(s.DynamicConfigValues)+1)
-	// Setting host level mutable state cache size to 8k.
-	dynConf[dynamicconfig.HistoryCacheHostLevelMaxSize.Key()] = 8096
-	// Up default visibility RPS
-	dynConf[dynamicconfig.FrontendMaxNamespaceVisibilityRPSPerInstance.Key()] = 100
-	// NOTE that the URL scheme is fixed to HTTP since the dev server doesn't support TLS at the time of writing.
-	dynConf[nexusoperations.CallbackURLTemplate.Key()] = fmt.Sprintf(
-		"http://%s:%d/namespaces/{{.NamespaceName}}/nexus/callback", MaybeEscapeIPv6(s.FrontendIP), s.FrontendHTTPPort)
-	dynConf[callbacks.AllowedAddresses.Key()] = []struct {
-		Pattern       string
-		AllowInsecure bool
-	}{
-		{
-			Pattern:       fmt.Sprintf("%s:%d", MaybeEscapeIPv6(s.FrontendIP), s.FrontendHTTPPort),
-			AllowInsecure: true,
-		},
-	}
-
-	// Dynamic config if set
-	for k, v := range s.DynamicConfigValues {
-		dynConf[dynamicconfig.MakeKey(k)] = v
-	}
-	opts = append(opts, temporal.WithDynamicConfigClient(dynConf))
-
-	// gRPC interceptors if set
-	if len(s.GRPCInterceptors) > 0 {
-		opts = append(opts, temporal.WithChainedFrontendGrpcInterceptors(s.GRPCInterceptors...))
-	}
-
-	return opts, logLevel, nil
-}
-
-func (s *StartOptions) buildServerConfig() (*config.Config, error) {
-	var conf config.Config
-	// Global config
-	conf.Global.Membership.MaxJoinDuration = 30 * time.Second
-	conf.Global.Membership.BroadcastAddress = s.FrontendIP
-	if conf.Global.Metrics == nil && s.MetricsPort > 0 {
-		conf.Global.Metrics = &metrics.Config{
-			Prometheus: &metrics.PrometheusConfig{
-				ListenAddress: fmt.Sprintf("%v:%v", MaybeEscapeIPv6(s.FrontendIP), s.MetricsPort),
-				HandlerPath:   "/metrics",
-			},
-		}
-	}
-	conf.Global.PProf.Port = s.PProfPort
-
-	// Persistence config
-	conf.Persistence.DefaultStore = "sqlite-default"
-	conf.Persistence.VisibilityStore = "sqlite-default"
-	conf.Persistence.NumHistoryShards = 1
-	sqlConf, err := s.buildSQLConfig()
-	if err != nil {
-		return nil, err
-	}
-	conf.Persistence.DataStores = map[string]config.DataStore{"sqlite-default": {SQL: sqlConf}}
-
-	// Other config
-	if conf.ClusterMetadata == nil {
-		conf.ClusterMetadata = &cluster.Config{
-			EnableGlobalNamespace:    s.EnableGlobalNamespace,
-			FailoverVersionIncrement: 10,
-			MasterClusterName:        s.MasterClusterName,
-			CurrentClusterName:       s.CurrentClusterName,
-			ClusterInformation: map[string]cluster.ClusterInformation{
-				s.CurrentClusterName: {
-					Enabled:                true,
-					InitialFailoverVersion: int64(s.InitialFailoverVersion),
-					RPCAddress:             fmt.Sprintf("%v:%v", MaybeEscapeIPv6(s.FrontendIP), s.FrontendPort),
-					HTTPAddress:            fmt.Sprintf("%v:%v", MaybeEscapeIPv6(s.FrontendIP), s.FrontendHTTPPort),
-					ClusterID:              s.ClusterID,
-				},
-			},
-		}
-	}
-	conf.DCRedirectionPolicy.Policy = "noop"
-	conf.Services = map[string]config.Service{
-		"frontend": s.buildServiceConfig(true),
-		"history":  s.buildServiceConfig(false),
-		"matching": s.buildServiceConfig(false),
-		"worker":   s.buildServiceConfig(false),
-	}
-	conf.Archival.History.State = "disabled"
-	conf.Archival.Visibility.State = "disabled"
-	conf.NamespaceDefaults.Archival.History.State = "disabled"
-	conf.NamespaceDefaults.Archival.Visibility.State = "disabled"
-	conf.PublicClient.HostPort = fmt.Sprintf("%v:%v", MaybeEscapeIPv6(s.FrontendIP), s.FrontendPort)
-	return &conf, nil
-}
-
-func (s *StartOptions) buildSQLConfig() (*config.SQL, error) {
-	conf := config.SQL{PluginName: sqliteplugin.PluginName}
-	if s.DatabaseFile == "" {
-		conf.ConnectAttributes = map[string]string{"mode": "memory", "cache": "shared"}
-		conf.DatabaseName = strconv.Itoa(rand.Intn(9999999))
-	} else {
-		conf.ConnectAttributes = map[string]string{"mode": "rwc"}
-		conf.DatabaseName = s.DatabaseFile
-	}
-	for k, v := range s.SqlitePragmas {
-		conf.ConnectAttributes[k] = v
-	}
-
-	// Apply migrations to sqlite if using file but it does not exist
-	if s.DatabaseFile != "" {
-		if _, err := os.Stat(s.DatabaseFile); os.IsNotExist(err) {
-			// Eagerly check parent dir
-			if _, err := os.Stat(filepath.Dir(s.DatabaseFile)); err != nil {
-				return nil, fmt.Errorf("failed checking dir for database file: %w", err)
-			}
-			if err := sqliteschema.SetupSchema(&conf); err != nil {
-				return nil, fmt.Errorf("failed setting up schema: %w", err)
-			}
-		}
-	}
-
-	// Create namespaces
-	namespaces := make([]*sqliteschema.NamespaceConfig, len(s.Namespaces))
-	for i, ns := range s.Namespaces {
-		nsConfig, err := sqlite.NewNamespaceConfig(s.CurrentClusterName, ns, false, s.SearchAttributes)
-		if err != nil {
-			return nil, fmt.Errorf("failed creating namespace config: %w", err)
-		}
-		namespaces[i] = nsConfig
-	}
-	if err := sqliteschema.CreateNamespaces(&conf, namespaces...); err != nil {
-		return nil, fmt.Errorf("failed creating namespaces: %w", err)
-	}
-	return &conf, nil
-}
-
-func (s *StartOptions) buildServiceConfig(frontend bool) config.Service {
-	var conf config.Service
-	if frontend {
-		conf.RPC.GRPCPort = s.FrontendPort
-		conf.RPC.BindOnIP = s.FrontendIP
-		conf.RPC.HTTPPort = s.FrontendHTTPPort
-	} else {
-		conf.RPC.GRPCPort = MustGetFreePort(s.FrontendIP)
-		conf.RPC.BindOnIP = s.FrontendIP
-	}
-	return conf
 }
